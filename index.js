8f896854ba80f681d5f856c35981505bfeacf2bb
LiZhaji <1157287181@qq.com>
2019-04-27 01:06:34 +0800

    'recode'

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const drugMe = (obj) => {
-    obj.onmousedown = function (event) {
-        //设置obj捕获所以鼠标摁下的事件，只有IE有此函数且renturn false对其无效
-        obj.setCapture && obj.setCapture();
-
-        // 计算鼠标和元素左上角的水平和垂直偏移量，否则点击鼠标默认会跳到元素左上角;兼容IE6
-        event = event || window.event
-        const displacementX =  event.clientX -  obj.offsetLeft
-        const displacementY=  event.clientY -  obj.offsetTop
-        document.onmousemove = function (event) {
-
-            event = event || window.event
-            var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft ;
-            var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-            var left = event.clientX - displacementX + scrollLeft
-            var top = event.clientY - displacementY + scrollTop
-            obj.style.left = left + 'px'
-            obj.style.top = top + 'px'
-        }
-        document.onmouseup = function () {
-            document.onmousemove = null
-            document.onmouseup = null
-        }
+const dragMe = (element, options = {}) => {
+    const handler = options.handler || element
+    let moving = false
+    let startMousePosition = {x: 0, y: 0}
+    let startElementPosition = {x: 0, y: 0}
+    const move = (event) => {
+        const moveX = event.clientX - startMousePosition.x
+        const moveY = event.clientY - startMousePosition.y
+        element.style.left = startElementPosition.x + moveX + 'px'
+        element.style.top = startElementPosition.y + moveY + 'px'
     }
-    // 取消浏览器的默认拖拽搜索行为
-    return false
+    handler.addEventListener('mousedown', function (event) {
+        moving = true
+        startMousePosition.x = event.clientX
+        startMousePosition.y = event.clientY
+        startElementPosition.x = parseInt(getComputedStyle(element).left)
+        startElementPosition.y = parseInt(getComputedStyle(element).top)
+    })
+    handler.addEventListener('mouseup', function (event) {
+        move(event)
+        moving = false
+    })
+    document.addEventListener('mousemove', function (event) {
+        if (!moving) return
+        move(event)
+    })
+
 }
-module.exports = drugMe
+export default dragMe
